c90445a8a1bcabdb285f6dc24cdeb299801a5e57
unknown <Akshatha.Hv@mindtree.com>
2018-02-02 14:44:11 +0530

    update with get info

---
+++ index.js
@@ -0,0 +1,15 @@
+exports.info = function () {
+    return [
+        {
+            "id": 1,
+            "name": "Devops"
+        }, {
+            "id": 2,
+            "name": "blockchain"
+        },
+        {
+            "id": 3,
+            "name": "kubernetes"
+        }
+    ]
+};
